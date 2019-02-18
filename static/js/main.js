021175be1476ba370681cd92d7c4cb20513d499e
JuanJoseConejos90 <45395653+JuanJoseConejos90@users.noreply.github.com>
2019-02-18 14:28:39 -0600

    Update main.js

--- static/js/main.js
+++ static/js/main.js
@@ -3,7 +3,7 @@ $(function () {
 });
 
 $('#fontTypeMenu').on('change', function () {
-    $(".fr-view").css({ 'font-family': 'Impact, Haettenschweiler, Arial Narrow Bold, sans-serif' });
+    $(".fr-view").css({ 'font-family': 'Impact, Haettenschweiler, Arial Narrow Bold, sans-serif !important' });
 });
 
 
@@ -13,50 +13,9 @@ $('#fontSizeMenu').on('change', function () {
 
 
 $("#btnPrimary").click(function () {
-
     printPage();
 });
 
-$('#exampleFormControlFile1').on('change', function () {
-    const file = this.files[0];
-    const reader = new FileReader();
-    reader.onload = (event) => {
-
-        var zip = new JSZip(event.target.result);
-        var doc=new window.docxtemplater().loadZip(zip);
-        var text=doc.getFullText();
-        console.log(text);
-        const file = event.target.result;
-        const allLines = file.split(/\r\n|\n/);
-        var limit = 30;
-        var i = 1;
-        var p = '';
-        var page = ["A", "B"];
-        var index = 0;
-        var pagenumber = 1;
-        $('.modal-body').empty();
-        allLines.forEach((line) => {
-            if (i <= limit) {
-                p += `<p class="${page[index] + i}">${line}</p>`
-                i++;
-            } else {
-                renderaddCard(p, page[index], pagenumber);
-                addContent(p)
-                i = 0;
-                p = '';
-                index = (index == 0) ? 1 : 0;
-                pagenumber++;
-
-            }
-        });
-    };
-
-    reader.onerror = (event) => {
-        alert(event.target.error.name);
-    };
-
-    reader.readAsText(file, "UTF-8");
-});
 
 
 
@@ -79,11 +38,7 @@ function renderContentPrint(p, pagina) {
     $('#containerPrint').append(p);
 }
 
-
-function setlistener() {
-
-
-}
+function setlistener() { }
 
 function renderaddCard(content, page, pagenumber) {
     var card = `<div class="card" data-number="${pagenumber}" data-page="${page}">` +
@@ -113,8 +68,10 @@ function setPrint() {
     }
 }
 
-
 function printPage() {
+
+    settingsPageLine();
+    settingsPaginado();
     $(".fr-view").print({
         globalStyles: false,
         mediaPrint: false,
@@ -129,16 +86,128 @@ function printPage() {
         title: null,
         doctype: '<!doctype html>'
     });
+
+    $(".fr-view").css({ "margin-top": "0mm" });
+    $(".fr-view").css({ "margin-left": "0mm" });
 }
 
+function settingsPaginado() {
+    var paginado = $('#Paginado')[0].value;
+    if (paginado == 'A') {
+        $(".fr-view").css({ "margin-left": "66px", "padding-right": "18px" });
+    } else {
+        //Set lado B
+        $(".fr-view").css({ "margin-left": "66px", "padding-right": "18px" });
+    }
+}
 
-function loadFile(url,callback){
-    JSZipUtils.getBinaryContent(url,callback);
+function settingsPageLine() {
+    var line = $('#linea')[0].value;
+    var marginTop = '';
+    switch (line) {
+        case "1":
+            marginTop = "36mm";
+            break;
+        case "2":
+            marginTop = "46mm";
+            break;
+        case "3":
+            marginTop = "56mm";
+            break;
+        case "4":
+            marginTop = "66mm";
+            break;
+        case "5":
+            marginTop = "76mm";
+            break;
+        case "6":
+            marginTop = "86mm";
+            break;
+        case "7":
+            marginTop = "96mm";
+            break;
+        case "8":
+            marginTop = "106mm";
+            break;
+        case "9":
+            marginTop = "116mm";
+            break;
+        case "10":
+            marginTop = "126mm";
+            break;
+        case "11":
+            marginTop = "136mm";
+            break;
+        case "12":
+            marginTop = "146mm";
+            break;
+        case "13":
+            marginTop = "156mm";
+            break;
+        case "14":
+            marginTop = "166mm";
+            break;
+        case "15":
+            marginTop = "176mm";
+            break;
+        case "16":
+            marginTop = "186mm";
+            break;
+        case "17":
+            marginTop = "196mm";
+            break;
+        case "18":
+            marginTop = "206mm";
+            break;
+        case "19":
+            marginTop = "216mm";
+            break;
+        case "20":
+            marginTop = "226mm";
+            break;
+        case "21":
+            marginTop = "236mm";
+            break;
+        case "22":
+            marginTop = "246mm";
+            break;
+        case "23":
+            marginTop = "256mm";
+            break;
+        case "24":
+            marginTop = "266mm";
+            break;
+        case "25":
+            marginTop = "276mm";
+            break;
+        case "26":
+            marginTop = "286mm";
+            break;
+        case "27":
+            marginTop = "296mm";
+            break;
+        case "28":
+            marginTop = "306mm";
+            break;
+        case "29":
+            marginTop = "316mm";
+            break;
+        case "30":
+            marginTop = "326mm";
+            break;
+    }
+
+    $(".fr-view").css("margin-top", marginTop);
+
+}
+
+function loadFile(url, callback) {
+    JSZipUtils.getBinaryContent(url, callback);
 }
 function generate() {
-    loadFile("https://docxtemplater.com/tag-example.docx",function(error,content){
+    loadFile("https://docxtemplater.com/tag-example.docx", function (error, content) {
         var zip = new JSZip(content);
-        var doc=new window.docxtemplater().loadZip(zip)
+        var doc = new window.docxtemplater().loadZip(zip)
         doc.setData({
             first_name: 'John',
             last_name: 'Doe',
@@ -156,15 +225,15 @@ function generate() {
                 stack: error.stack,
                 properties: error.properties,
             }
-            console.log(JSON.stringify({error: e}));
+            console.log(JSON.stringify({ error: e }));
             // The error thrown here contains additional information when logged with JSON.stringify (it contains a property object).
             throw error;
         }
-        var out=doc.getZip().generate({
-            type:"blob",
+        var out = doc.getZip().generate({
+            type: "blob",
             mimeType: "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
         }) //Output the document using Data-URI
-        saveAs(out,"output.docx")
+        saveAs(out, "output.docx")
     })
 }
 
